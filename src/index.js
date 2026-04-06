77642b4f76ac0b7774aec8ee5773c4078f3839c2
Adnan <adnanalishah761@gmail.com>
2026-04-06 09:53:34 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import ImageGen from "./components/ImageCard";
+import Body from "./components/Body";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Body />);
+ 
